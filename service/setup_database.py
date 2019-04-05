9786f90335e62212c5dc3c5d1d08a5a1ea87af0d
Harikrishnan Narasiman <harikrish@Harikrishnans-MacBook-Pro.local>
2019-04-04 21:48:05 -0400

    Initial Commit

---
+++ service/setup_database.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy import create_engine
+# from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+import os
+
+Base = declarative_base()
+
+class User(Base):
+
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    username = Column(String(80),nullable=False, unique=True)
+    password_hash = Column(String(80))
+    picture = Column(String(80))
+    email = Column(String(100), nullable=False)
+
+database_url = 'postgresql://couchmaster:Welcome12#@couchbumsdb-dev.cyrebkvrvdix.us-east-1.rds.amazonaws.com:5432/couchbums'
+engine = create_engine(database_url)
+
+Base.metadata.create_all(engine)
